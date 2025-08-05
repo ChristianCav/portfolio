89b6778f27324d361b1b26791edae8ad645ea2ef
ChristianCav <christiancav55@gmail.com>
2025-08-05 16:36:24 -0400

    setup

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css';
+import Navbar from './components/Navbar';
+
+function App() {
+  return (
+    <div className="relative h-full w-full">
+      <Navbar></Navbar>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/Navbar.tsx
@@ -0,0 +1,11 @@
+const Navbar = () => {
+  return (
+    <div className="flex items-center justify-center pt-2">
+      <button className="btn btn-sm">Home</button>
+      <button className="btn btn-sm">Projects</button>
+      <button className="btn btn-sm">About Me</button>
+    </div>
+  );
+};
+
+export default Navbar;
